15c378d980fa3fbd10adb765b594f1a782d4393e
進捗ゼミ <47470998+wolfmagnate@users.noreply.github.com>
2025-06-19 11:39:44 +0900

    add sample main

---
+++ backend/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	// Echoのインスタンスを作成
+	e := echo.New()
+
+	// ミドルウェアを設定（推奨）
+	// リクエストID、ロガー、パニックからの復帰など
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// ルーティング: GET / で "hello world" を返す
+	e.GET("/", func(c echo.Context) error {
+		// c.String(ステータスコード, 返す文字列)
+		return c.String(http.StatusOK, "hello world")
+	})
+
+	// Webサーバーをポート8080で起動
+	// e.Logger.Fatal はエラーが発生した場合にログを出力してプログラムを終了します
+	e.Logger.Fatal(e.Start(":8080"))
+}
